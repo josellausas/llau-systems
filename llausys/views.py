a62b8a7a6c8bfc0ea6b1793270464b91fc32358b
zunware <jose@josellausas.com>
2019-10-02 17:59:24 -0500

    Added sections, views and templates

--- llausys/views.py
+++ llausys/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def techs(request):
+    return render(request, 'techs.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
