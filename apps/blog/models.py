d888ddeea519d73d5f933d34d157c7928ce59055
zunware <jose@josellausas.com>
2019-10-03 01:05:12 -0500

    Renamed author

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+User = settings.AUTH_USER_MODEL
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(null=True, blank=True)
-    created_by = models.ForeignKey(
+    author = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
         null=True,
-        blank=True
+        blank=True,
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
