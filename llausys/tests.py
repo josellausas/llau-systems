83276b68cfe40203a1069c985d3fb5c1cb958a7f
zunware <jose@josellausas.com>
2019-10-02 19:00:01 -0500

    Tests and sections

--- llausys/tests.py
+++ llausys/tests.py
@@ -4,13 +4,11 @@ class LlauSysHomeTests(TestCase):
     def setUp(self):
         self.response = self.client.get("/")
 
-    def test_home_exists(self):
-        self.assertEquals(200, self.response.status_code)
-
     def test_home_menu(self):
         """Check that all menu options exist"""
         # Check that the rendered context contains 5 customers.
         # self.assertEqual(len(response.context['customers']), 5)
+        self.assertEquals(200, self.response.status_code)
         content = self.response.content.decode("utf-8")
         self.assertIn("Home", content)
         self.assertIn("Services", content)
@@ -19,16 +17,60 @@ class LlauSysHomeTests(TestCase):
         self.assertIn("Blog", content)
         self.assertIn("Contact", content)
 
+
 class LlauSysServicesTests(TestCase):
     def setUp(self):
         self.response = self.client.get("/services", follow=True)
 
-    def test_services_exists(self):
-        self.assertEquals(200, self.response.status_code)
-
     def test_services_content(self):
+        """Check that all menu options exist"""
+        self.assertEquals(200, self.response.status_code)
+        content = self.response.content.decode("utf-8")
+        self.assertIn("Services", content)
+
+
+class LlauSysProjectsTests(TestCase):
+    def setUp(self):
+        self.response = self.client.get("/projects", follow=True)
+
+    def test_projects_content(self):
+        """Check that all menu options exist"""
+        self.assertEquals(200, self.response.status_code)
+        content = self.response.content.decode("utf-8")
+        self.assertIn("Projects</h2>", content)
+
+class LlauSysTechnologiesTests(TestCase):
+    def setUp(self):
+        self.response = self.client.get("/tech", follow=True)
+
+    def test_tech_content(self):
         """Check that all menu options exist"""
         # Check that the rendered context contains 5 customers.
         # self.assertEqual(len(response.context['customers']), 5)
+        self.assertEquals(200, self.response.status_code)
         content = self.response.content.decode("utf-8")
-        self.assertIn("Services", content)
+        self.assertIn("Technologies</h2>", content)
+
+class LlauSysBlogTests(TestCase):
+    def setUp(self):
+        self.response = self.client.get("/blog", follow=True)
+
+    def test_tech_content(self):
+        """Check that all menu options exist"""
+        # Check that the rendered context contains 5 customers.
+        # self.assertEqual(len(response.context['customers']), 5)
+        self.assertEquals(200, self.response.status_code)
+        content = self.response.content.decode("utf-8")
+        self.assertIn("Blog</h2>", content)
+
+class LlauSysContactTests(TestCase):
+    def setUp(self):
+        self.response = self.client.get("/contact", follow=True)
+
+    def test_tech_content(self):
+        """Check that all menu options exist"""
+        # Check that the rendered context contains 5 customers.
+        # self.assertEqual(len(response.context['customers']), 5)
+        self.assertEquals(200, self.response.status_code)
+        content = self.response.content.decode("utf-8")
+        self.assertIn("Contact</h2>", content)
