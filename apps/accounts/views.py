4fdac30a00d457dbd2915402b821b5b45b0b8676
zunware <jose@josellausas.com>
2019-10-06 15:53:54 -0500

    Added signup

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -7,6 +7,14 @@ from .forms import UserLoginForm, UserRegisterForm
 
 def signup_view(request):
     form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get("password")
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        return redirect("profile")
     context = {
         "title": "Sign Up",
         "form": form
