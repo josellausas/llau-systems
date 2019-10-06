dd2590e813ef2317c59251835de789d134facafa
zunware <jose@josellausas.com>
2019-10-06 15:15:52 -0500

    Added login view

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, get_user_model, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+from .forms import UserLoginForm
 
 
 def signup_view(request):
@@ -7,10 +11,44 @@ def signup_view(request):
 
 
 def login_view(request):
-    context = {}
-    return render(request, 'accounts/login.html', context)
+    if request.user.is_authenticated:
+        print("redirecting")
+        return redirect("/profile")
+
+    # TODO: Log login view
+    form = UserLoginForm(request.POST or None)
+    # TODO: Log POST attempt
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        # This only checks for valid creds
+        user = authenticate(username=username, password=password)
+        if user:
+            # This logins a user with valid creds
+            login(request, user)
+            print(request.user.is_authenticated)
+            
+    else:
+        # TODO: Log bad attempt here
+        pass
+
+    context = {
+        "title": "Log In",
+        "subtitle": "Auth yourself",
+        "form": form,
+    }
+    return render(request, 'accounts/form.html', context)
 
 
+@login_required
 def logout_view(request):
     context = {}
     return render(request, 'accounts/logout.html', context)
+
+
+@login_required
+def my_profile_view(request):
+    context = {
+        'title': "Profile"
+    }
+    return render(request, 'accounts/profile.html', context)
--- llausys/urls.py
+++ llausys/urls.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from . import views
 from apps.blog.views import blog_post_create
-from apps.accounts.views import login_view, logout_view, signup_view
+from apps.accounts.views import (
+    login_view, logout_view, signup_view, my_profile_view
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +14,7 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('signup/', signup_view, name='signup'),
+    path('profile/', my_profile_view, name='profile'),
     path('blog/', include('apps.blog.urls', namespace='blog')),
     path('blog-new/', blog_post_create),
     re_path(r'^services?/$', views.services),
