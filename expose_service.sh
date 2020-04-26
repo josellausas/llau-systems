c5c00e77199e1b0368b3d86877c559e7e7f718c1
zunware <jose@josellausas.com>
2020-04-26 11:32:28 -0500

    OK

---
+++ expose_service.sh
@@ -0,0 +1,1 @@
+kubectl expose deployment llausys-web --type=LoadBalancer --name=web-service
--- llausys/settings.py
+++ llausys/settings.py
@@ -17,13 +17,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
+HOST = env('ALLOWED_HOST')
 
 ALLOWED_HOSTS = [
-    '*',
+    HOST
 ]
 
 INTERNAL_IPS = (
-    '*',
+    '*'
 )
 
 LOGIN_URL = '/login/'
