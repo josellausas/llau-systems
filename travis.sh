f17574c013e7c22a8be17bcbf416f1365469100c
zunware <jose@josellausas.com>
2020-05-04 23:49:23 -0500

    More travis

--- travis.sh
+++ travis.sh
@@ -1,8 +1,8 @@
 #/usr/bin/env bash
 curl -sL https://sentry.io/get-cli/ | bash
 VERSION=$(sentry-cli releases propose-version)
-sentry-cli releases new -p llau-systems-web $VERSION
-sentry-cli releases set-commits --commit "josellausas/llau-systems@$TRAVIS_COMMIT_RANGE"
+sentry-cli releases new -p llau-systems-web $VERSION && \
+sentry-cli releases set-commits --commit "josellausas/llau-systems@$TRAVIS_COMMIT_RANGE" $VERION && \
 ./test.sh && \
 docker build -t zunware/llau-systems-web . && \
 ./push.sh
